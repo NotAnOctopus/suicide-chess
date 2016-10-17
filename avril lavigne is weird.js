5b6b0a2e7e02b383e3af038dc58c7b80282577cd
NotAnOctopus <zapmeister999@hotmail.co.uk>
2016-10-17 01:14:43 +0100

    Update avril lavigne is weird.js

--- avril lavigne is weird.js
+++ avril lavigne is weird.js
@@ -13,6 +13,7 @@ function loadXMLDoc(file) {
     if (this.readyState == 4 && this.status == 200) {
       cats_are_stupid = this.responseText;
       // document.getElementById("bananas").innerHTML = cats_are_stupid;
+      cats_are_stupid=cats_are_stupid.split('\n');
     }
   };
   xmlhttp.open("GET", file, true);
@@ -21,4 +22,14 @@ function loadXMLDoc(file) {
 
 function thingy() {
   document.getElementById("bananas").innerHTML = cats_are_stupid;
+}
+
+function cookie_clicker(move) {
+  var homer_simpson = '';
+  for (var k=0; k<cats_are_stupid.length; k++) {
+    if (cats_are_stupid[k].startsWith('e2e3 '+move)) {
+      homer_simpson+=cats_are_stupid[k]+'<br>';
+    }
+  }
+  document.getElementById('bananas').innerHTML = homer_simpson;
 }
