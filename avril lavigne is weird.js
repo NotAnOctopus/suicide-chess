62f1431229bab23c4170037da95fb33f1b0703ef
NotAnOctopus <zapmeister999@hotmail.co.uk>
2016-10-19 23:59:27 +0100

    added restart functionality

--- avril lavigne is weird.js
+++ avril lavigne is weird.js
@@ -69,3 +69,34 @@ function magic(movelist) {
     lasers_are_cool.appendChild(butter);
   }
 }
+
+function badger(thing, thingy, thingything) { // this and the function below it restarts the search
+  var butter=document.createElement("BUTTON");
+  var margarine=document.createTextNode(thingything);
+  butter.onclick = function (yourface) {
+    getthispartystarted(thing,thingy);
+  };
+  butter.appendChild(margarine);
+  lasers_are_really_cool = document.getElementById("emmawatson");
+  lasers_are_really_cool.appendChild(butter);
+}
+
+function badgerbadgerbadger() { // mushroom mushroom
+  cats_are_stupid=[];
+  rainbow_unicorns=['e2e3'];
+  document.getElementById("spinning_around_on_office_chairs_is_cool").innerHTML = "Move list: e2e3";
+  document.getElementById("something").innerHTML = "available moves go here";
+  document.getElementById("emmawatson").innerHTML = "";
+  badger('easy.txt', 'a7a5', 'a5');
+  badger('easy.txt', 'a7a6', 'a6');
+  badger('andryushkov.txt', 'c7c6', 'c6');
+  badger('easy.txt', 'e7e5', 'e5');
+  badger('easy.txt', 'f7f5', 'f5');
+  badger('easy.txt', 'f7f6', 'f6');
+  badger('easy.txt', 'g7g6', 'g6');
+  badger('easy.txt', 'h7h5', 'h5');
+  badger('easy.txt', 'h7h6', 'h6');
+  badger('easy.txt', 'b8a6', 'Na6');
+  badger('balkan.txt', 'b8c6', 'Nc6');
+  badger('easy.txt', 'g8f6', 'Nf6');
+}
